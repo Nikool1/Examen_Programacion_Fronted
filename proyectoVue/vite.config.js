8e774484e3bed456026f474113f73e9a3b56bfc7
Nikool1 <nicolas.m.aburto@gmail.com>
2025-07-21 18:32:05 -0400

    Carga de proyecto 3 FIX

--- proyectoVue/vite.config.js
+++ proyectoVue/vite.config.js
@@ -1,18 +1,17 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: './', // ← esta línea es CLAVE para que funcione en Netlify (ajusta a '/' si usas rutas absolutas)
   plugins: [
     vue(),
-    vueDevTools(),
+    vueDevTools()
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+    }
+  }
 })
